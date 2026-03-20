a05b9bc47baeb978ca6df7a3d65c9ba3debcf20c
Renz Dadpaas <rdadpaas123@gmail.com>
2026-03-20 16:46:21 +0800

    created Word class to represent every word used by a player containing effects, parts of speech, and its magnitude

--- word/Word.cpp
+++ word/Word.cpp
@@ -1,5 +1,26 @@
 //
 // Created by ASUS TUF F15 on 3/18/2026.
 //
-
 #include "Word.h"
+
+Word::Word(const std::string& text, PartOfSpeech pos, EffectTag effect, int effectValue)
+    : text(text), pos(pos), effect(effect), effectValue(effectValue) 
+{
+
+}
+
+const std::string& Word::getText() const {
+    return text;
+}
+
+PartOfSpeech Word::getPartOfSpeech() const {
+    return pos;
+}
+
+EffectTag Word::getEffectTag() const {
+    return effect;
+}
+
+int Word::getEffectValue() const {
+    return effectValue;
+}
--- word/Word.h
+++ word/Word.h
@@ -5,9 +5,44 @@
 #ifndef UNTITLED1_WORD_H
 #define UNTITLED1_WORD_H
 
+#include <string>
 
-class Word {
+// 'enum class' provides strong typing (prevents implicit conversions to integers).
+// This makes the code safer and easier to read.
+enum class PartOfSpeech {
+    Noun,
+    Verb,
+    Adjective,
+    Adverb,
+    Pronoun,
+    Article,
+    Preposition,
+    Conjunction
 };
 
+// Represents the effect a word can trigger when used in a valid sentence.
+enum class EffectTag {
+    None,
+    Offense,
+    Defense,
+    Critical,
+    Heal
+};
+
+class Word {
+private:
+    std::string text;
+    PartOfSpeech pos;
+    EffectTag effect;
+    int effectValue;
+
+public:
+    Word(const std::string& text, PartOfSpeech pos, EffectTag effect, int effectValue);
+
+    const std::string& getText() const;
+    PartOfSpeech getPartOfSpeech() const;
+    EffectTag getEffectTag() const;
+    int getEffectValue() const;
+};
 
 #endif //UNTITLED1_WORD_H
