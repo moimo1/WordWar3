ce898743009511c679a315bce2791a9b03956e59
Renz Dadpaas <rdadpaas123@gmail.com>
2026-03-25 18:02:30 +0800

    transfered game logic from Server to GameManager

--- network/Server.cpp
+++ network/Server.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <thread>
-#include "../word/WordDictionary.h"
+#include "../manager/GameManager.h"
 
 using namespace nlohmann;
 using asio::ip::tcp;
@@ -10,9 +10,6 @@ using asio::ip::tcp;
 std::mutex dbMutex;
 
 Server::Server(short port) : io_context(), acceptor(io_context, tcp::endpoint(tcp::v4(), port)) {
-    validFormulas = {
-        "NV", "ANV", "AJNV", "PV", "NVD", "ANVD", "PVD", "NVRN", "PVRN", "ANVRAN", "V", "VD", "NVC", "PVA"
-    };
 }
 
 void Server::sendPacket(tcp::socket& socket, const Packet& packet) {
@@ -78,24 +75,6 @@ void Server::recordMatchResult(const std::string& p1Name, const std::string& p2N
         accountsDb[p1Name]["losses"] = (int)accountsDb[p1Name]["losses"] + 1;
     }
     saveDatabase();
-}
-
-void Server::processTurn(std::shared_ptr<tcp::socket> attackerSock, std::shared_ptr<tcp::socket> defenderSock, 
-                         Player& attacker, Player& defender, const Sentence& sent) {
-    std::string sig = sent.getPatternSignature();
-    if (validFormulas.contains(sig)) {
-        sendPacket(*attackerSock, Packet{PacketType::SERVER_LOG_MESSAGE, "Valid Syntax! [" + sig + "] Attack lands!"});
-        sendPacket(*defenderSock, Packet{PacketType::SERVER_LOG_MESSAGE, "Opponent attacked successfully!"});
-        
-        std::vector<std::string> logs = attacker.applySentenceEffects(sent, defender);
-        for (const auto& logMsg : logs) {
-            sendPacket(*attackerSock, Packet{PacketType::SERVER_LOG_MESSAGE, logMsg});
-            sendPacket(*defenderSock, Packet{PacketType::SERVER_LOG_MESSAGE, logMsg});
-        }
-    } else {
-        sendPacket(*attackerSock, Packet{PacketType::SERVER_LOG_MESSAGE, "Invalid Grammar! [" + sig + "] Attack FIZZLED!"});
-        sendPacket(*defenderSock, Packet{PacketType::SERVER_LOG_MESSAGE, "Opponent mumbled gibberish. Nothing happens."});
-    }
 }
 
 void Server::run() {
@@ -247,119 +226,25 @@ void Server::matchmakerLoop() {
         
         std::cout << "Match Found computationally! " << p1->username << " vs " << p2->username << "\n";
         
-        // Spawn entirely isolated dedicated Match Instance matrix loop!
+        // Spawn entirely isolated GameManager structural matrix naturally!
         std::thread([this, p1, p2]() {
-            runMatchInstance(p1, p2);
+            std::cout << "Initializing internal GameManager structurally natively for " << p1->username << " and " << p2->username << "...\n";
+            GameManager game(p1, p2);
+            int outcome = game.runMatch();
+            
+            if (outcome == 1) recordMatchResult(p1->username, p2->username, true, false);
+            else if (outcome == 2) recordMatchResult(p1->username, p2->username, false, true);
+            else if (outcome == 0) recordMatchResult(p1->username, p2->username, false, false);
+
+            std::cout << "Match End. Structurally spawning isolated Post-Match listener blocks natively...\n";
+            std::thread([this, p1]() {
+                broadcastLeaderboard(p1->socket);
+                authenticate(p1->socket);
+            }).detach();
+            std::thread([this, p2]() {
+                broadcastLeaderboard(p2->socket);
+                authenticate(p2->socket);
+            }).detach();
         }).detach();
     }
-}
-
-void Server::runMatchInstance(std::shared_ptr<PlayerSession> p1Session, std::shared_ptr<PlayerSession> p2Session) {
-    WordDictionary dict("../database/words.json");
-    Player p1(p1Session->username, 200);
-    Player p2(p2Session->username, 200);
-
-    auto sock1 = p1Session->socket;
-    auto sock2 = p2Session->socket;
-
-    try {
-        sendPacket(*sock1, Packet{PacketType::SERVER_MATCH_FOUND, {{"opponent", p2Session->username}, {"my_max_hp", p1.getMaxHp()}, {"opp_max_hp", p2.getMaxHp()}}});
-    } catch (...) {
-        std::cout << p1.getName() << " was a dead connection! Aborting.\n";
-        try { sendPacket(*sock2, Packet{PacketType::GAME_OVER, "DRAW"}); } catch(...) {}
-        return;
-    }
-
-    try {
-        sendPacket(*sock2, Packet{PacketType::SERVER_MATCH_FOUND, {{"opponent", p1Session->username}, {"my_max_hp", p2.getMaxHp()}, {"opp_max_hp", p1.getMaxHp()}}});
-    } catch (...) {
-        std::cout << p2.getName() << " was a dead connection! Aborting.\n";
-        try { sendPacket(*sock1, Packet{PacketType::GAME_OVER, "DRAW"}); } catch(...) {}
-        return;
-    }
-
-    try {
-        while (p1.getHp() > 0 && p2.getHp() > 0) {
-            std::vector<Word> pool1 = dict.getRandomWords(12);
-            std::vector<Word> pool2 = dict.getRandomWords(12);
-
-            json j1 = json::array(), j2 = json::array();
-            for(auto& w : pool1) j1.push_back(w.toJson());
-            for(auto& w : pool2) j2.push_back(w.toJson());
-
-            sendPacket(*sock1, Packet{PacketType::SERVER_SEND_POOL, j1});
-            sendPacket(*sock2, Packet{PacketType::SERVER_SEND_POOL, j2});
-
-            Packet p1Turn = receivePacket(*sock1);
-            Sentence sent1;
-            for(auto& jObj : p1Turn.payload) sent1.addWord(Word::fromJson(jObj));
-            
-            Packet p2Turn = receivePacket(*sock2);
-            Sentence sent2;
-            for(auto& jObj : p2Turn.payload) sent2.addWord(Word::fromJson(jObj));
-
-            int len1 = sent1.getWords().size();
-            int len2 = sent2.getWords().size();
-
-            if (len1 < len2) {
-                sendPacket(*sock1, Packet{PacketType::SERVER_LOG_MESSAGE, "-> INITIATIVE! Your combo was tighter ("+std::to_string(len1)+" vs "+std::to_string(len2)+"). You strike first!"});
-                sendPacket(*sock2, Packet{PacketType::SERVER_LOG_MESSAGE, "-> OUTSPED! Opponent's fast combo strikes first!"});
-                processTurn(sock1, sock2, p1, p2, sent1);
-                if (p2.getHp() > 0) processTurn(sock2, sock1, p2, p1, sent2);
-            } else if (len2 < len1) {
-                sendPacket(*sock2, Packet{PacketType::SERVER_LOG_MESSAGE, "-> INITIATIVE! Your combo was tighter ("+std::to_string(len2)+" vs "+std::to_string(len1)+"). You strike first!"});
-                sendPacket(*sock1, Packet{PacketType::SERVER_LOG_MESSAGE, "-> OUTSPED! Opponent's fast combo strikes first!"});
-                processTurn(sock2, sock1, p2, p1, sent2);
-                if (p1.getHp() > 0) processTurn(sock1, sock2, p1, p2, sent1);
-            } else {
-                sendPacket(*sock1, Packet{PacketType::SERVER_LOG_MESSAGE, "-> CLASH! Perfect mirrors. Simultaneous strike!"});
-                sendPacket(*sock2, Packet{PacketType::SERVER_LOG_MESSAGE, "-> CLASH! Perfect mirrors. Simultaneous strike!"});
-                processTurn(sock1, sock2, p1, p2, sent1);
-                processTurn(sock2, sock1, p2, p1, sent2);
-            }
-
-            json jStatsP1 = {
-                {"my_hp", p1.getHp()}, {"my_shield", p1.getShield()},
-                {"opp_hp", p2.getHp()}, {"opp_shield", p2.getShield()}
-            };
-            json jStatsP2 = {
-                {"my_hp", p2.getHp()}, {"my_shield", p2.getShield()},
-                {"opp_hp", p1.getHp()}, {"opp_shield", p1.getShield()}
-            };
-            
-            sendPacket(*sock1, Packet{PacketType::SERVER_UPDATE_HP, jStatsP1});
-            sendPacket(*sock2, Packet{PacketType::SERVER_UPDATE_HP, jStatsP2});
-        }
-        
-        if (p1.getHp() <= 0 && p2.getHp() > 0) {
-            sendPacket(*sock1, Packet{PacketType::GAME_OVER, "DEFEAT"});
-            sendPacket(*sock2, Packet{PacketType::GAME_OVER, "VICTORY"});
-            recordMatchResult(p1.getName(), p2.getName(), false, true);
-        } else if (p2.getHp() <= 0 && p1.getHp() > 0) {
-            sendPacket(*sock1, Packet{PacketType::GAME_OVER, "VICTORY"});
-            sendPacket(*sock2, Packet{PacketType::GAME_OVER, "DEFEAT"});
-            recordMatchResult(p1.getName(), p2.getName(), true, false);
-        } else {
-            sendPacket(*sock1, Packet{PacketType::GAME_OVER, "DRAW"});
-            sendPacket(*sock2, Packet{PacketType::GAME_OVER, "DRAW"});
-            recordMatchResult(p1.getName(), p2.getName(), false, false);
-        }
-
-    } catch (...) {
-        std::cout << "Match abnormally terminated! Sockets lost actively connecting parameters!\n";
-        try { sendPacket(*sock1, Packet{PacketType::GAME_OVER, "VICTORY"}); } catch(...) {}
-        try { sendPacket(*sock2, Packet{PacketType::GAME_OVER, "VICTORY"}); } catch(...) {}
-        return; 
-    }
-
-    std::cout << "Match End. Structurally spawning isolated Post-Match listener blocks natively...\n";
-
-    std::thread([this, p1Session]() {
-        broadcastLeaderboard(p1Session->socket);
-        authenticate(p1Session->socket);
-    }).detach();
-    std::thread([this, p2Session]() {
-        broadcastLeaderboard(p2Session->socket);
-        authenticate(p2Session->socket);
-    }).detach();
 }
--- network/Server.h
+++ network/Server.h
@@ -33,27 +33,23 @@ private:
     std::mutex queueMutex;
     std::condition_variable queueCV;
 
-    std::set<std::string> validFormulas;
-
-    static void sendPacket(asio::ip::tcp::socket& socket, const Packet& packet);
-    static Packet receivePacket(asio::ip::tcp::socket& socket);
-
     void loadDatabase();
     void saveDatabase();
     
     void broadcastLeaderboard(std::shared_ptr<asio::ip::tcp::socket> sock);
     void recordMatchResult(const std::string& p1Name, const std::string& p2Name, bool p1Won, bool p2Won);
-    void processTurn(std::shared_ptr<asio::ip::tcp::socket> attackerSock, std::shared_ptr<asio::ip::tcp::socket> defenderSock, Player& attacker, Player& defender, const Sentence& sent);
 
     // Advanced Event-Driven Thread Handlers
     void acceptorLoop();
     void matchmakerLoop();
     void authenticate(std::shared_ptr<asio::ip::tcp::socket> sock);
-    void runMatchInstance(std::shared_ptr<PlayerSession> p1, std::shared_ptr<PlayerSession> p2);
 
 public:
     Server(short port);
     void run();
+    
+    static void sendPacket(asio::ip::tcp::socket& socket, const Packet& packet);
+    static Packet receivePacket(asio::ip::tcp::socket& socket);
 };
 
 #endif // SERVER_H
